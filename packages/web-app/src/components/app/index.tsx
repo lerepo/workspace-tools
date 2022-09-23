2938ae461b76873e0fff33fbc162583a712d9dfc
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2022-09-23 15:57:48 +0400

    chore: remove unused import

--- packages/web-app/src/components/app/index.tsx
+++ packages/web-app/src/components/app/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 import { CssBaseline, ThemeProvider } from '@material-ui/core';
 import { styled } from '@material-ui/core/styles';
